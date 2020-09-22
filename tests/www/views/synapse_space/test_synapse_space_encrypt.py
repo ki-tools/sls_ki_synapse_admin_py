487e5513479ce1ab12b8ca776355dc8946342a2c
Patrick <pcstout@gmail.com>
2020-09-22 15:28:15 -0600

    Upgrade Python and synapseclient. Add flask-talisman

--- tests/www/views/synapse_space/test_synapse_space_encrypt.py
+++ tests/www/views/synapse_space/test_synapse_space_encrypt.py
@@ -10,9 +10,9 @@ def url_path():
 def test_it_redirects_to_login(client, url_path):
     res = client.get(url_path)
     assert res.status_code == 302
-    assert res.location.endswith('/login?next=%2Fsynapse_space%2Fencrypt')
+    assert res.location.endswith('/synapse_space/encrypt')
 
 
 def test_it_loads_the_page(client, url_path):
-    res = client.get(url_path)
+    res = client.get(url_path, follow_redirects=True)
     assert res.status_code == 200
